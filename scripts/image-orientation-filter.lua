745adbe31100c7c43a4c5b3e9aa040c479891050
Nicholas <nicholas@tinkertank.rocks>
2025-04-01 02:40:44 +1100

    refactoring image process

--- scripts/image-orientation-filter.lua
+++ scripts/image-orientation-filter.lua
@@ -1,3 +1,314 @@
+-- Image Orientation Filter for Pandoc
+-- This script processes images with orientation attributes and generates
+-- appropriate LaTeX code for scientific and academic templates.
+
+-- Debug mode - set to true for extra logging
+local DEBUG = false
+
+-- Helper function for debug logging
+local function debug(msg)
+  if DEBUG then
+    io.stderr:write("[Image Filter Debug] " .. msg .. "\n")
+  end
+end
+
+-- Helper function to escape LaTeX special characters
+local function escape_latex(str)
+  -- Escape backslashes first (to prevent double-escaping)
+  str = str:gsub("\\", "\\textbackslash{}")
+  
+  -- Escape other special LaTeX characters
+  str = str:gsub("([#$%%&_{}^~])", "\\%1")
+  
+  -- Handle special brackets and braces
+  str = str:gsub("<", "\\textless{}")
+  str = str:gsub(">", "\\textgreater{}")
+  str = str:gsub("|", "\\textbar{}")
+  
+  return str
+end
+
+-- Helper function to safely handle file paths for LaTeX
+local function safe_path(path)
+  -- Extract just the filename from path (ignore directory)
+  local filename = path:match("([^/\\]+)$") or path
+  
+  -- If the filename looks like a hash (common with Pandoc temp files),
+  -- wrap it in braces to prevent LaTeX from interpreting it
+  if filename:match("^[a-f0-9]+%.%w+$") then
+    debug("Detected hash-based filename: " .. filename)
+    return "{" .. escape_latex(filename) .. "}"
+  end
+  
+  -- Otherwise just escape special characters
+  return escape_latex(filename)
+end
+
+-- Detect template type based on meta information
+local function detect_template(meta)
+  -- Check for scientific template indicators
+  if meta.documentclass and meta.documentclass == "article" then
+    debug("Detected scientific template")
+    return "scientific"
+  end
+  
+  -- Check for academic template indicators
+  if meta.documentclass and meta.documentclass == "report" then
+    debug("Detected academic template")
+    return "academic"
+  end
+  
+  -- Default to scientific template
+  debug("No specific template detected, using scientific as default")
+  return "scientific"
+end
+
+-- Get a label from the image attributes or generate one from filename
+local function get_label(img)
+  -- Try to get label from identifier
+  if img.identifier and img.identifier ~= "" then
+    return img.identifier
+  end
+  
+  -- Try to get label from attributes
+  if img.attributes and img.attributes.id then
+    return img.attributes.id
+  end
+  
+  -- Generate label from filename
+  local filename = img.src:match("([^/\\]+)%.[^.]+$") or "img"
+  return "fig:" .. filename:gsub("%s+", "_"):lower()
+end
+
+-- Generate LaTeX figure code for scientific template
+local function scientific_figure(img, orientation)
+  local width = img.attributes.width or "0.8\\textwidth"
+  local label = get_label(img)
+  local caption = img.caption and pandoc.utils.stringify(img.caption) or ""
+  local path = safe_path(img.src)
+  
+  debug("Scientific template figure: " .. orientation .. " orientation")
+  
+  if orientation == "landscape" then
+    return string.format("\\landscapefigure{%s}{%s}{%s}{%s}",
+                         escape_latex(label),
+                         escape_latex(caption),
+                         width,
+                         path)
+  else
+    return string.format("\\portraitfigure{%s}{%s}{%s}{%s}",
+                         escape_latex(label),
+                         escape_latex(caption),
+                         width,
+                         path)
+  end
+end
+
+-- Generate LaTeX figure code for academic template
+local function academic_figure(img, orientation)
+  local width = img.attributes.width or "0.8\\textwidth"
+  local label = get_label(img)
+  local caption = img.caption and pandoc.utils.stringify(img.caption) or ""
+  local path = safe_path(img.src)
+  
+  debug("Academic template figure: " .. orientation .. " orientation")
+  
+  if orientation == "landscape" then
+    return string.format("\\landscapefigure{%s}{%s}{%s}{%s}",
+                         width,
+                         escape_latex(label),
+                         escape_latex(caption),
+                         path)
+  else
+    return string.format("\\portraitfigure{%s}{%s}{%s}{%s}",
+                         width,
+                         escape_latex(label),
+                         escape_latex(caption),
+                         path)
+  end
+end
+
+-- Main image processing function
+local function process_image(img, meta)
+  -- Skip processing if no orientation is specified
+  if not img.attributes or not img.attributes.orientation then
+    debug("No orientation specified for image: " .. img.src)
+    return nil
+  end
+
+  debug("Processing image: " .. img.src)
+  
+  -- Get orientation from attributes
+  local orientation = img.attributes.orientation:lower()
+  if orientation ~= "landscape" and orientation ~= "portrait" then
+    debug("Invalid orientation: " .. orientation .. ", defaulting to portrait")
+    orientation = "portrait"
+  end
+  
+  -- Detect template type
+  local template_type = detect_template(meta)
+  
+  -- Generate appropriate LaTeX code based on template type
+  local latex_code
+  if template_type == "scientific" then
+    latex_code = scientific_figure(img, orientation)
+  else
+    latex_code = academic_figure(img, orientation)
+  end
+  
+  debug("Generated LaTeX code: " .. latex_code)
+  
+  -- Return as a raw block for block-level images or raw inline for inline images
+  if img.attributes.placement and img.attributes.placement == "inline" then
+    return pandoc.RawInline("latex", latex_code)
+  else
+    return pandoc.RawBlock("latex", latex_code)
+  end
+end
+
+-- Register the filter
+return {
+  {
+    Meta = function(meta)
+      -- Store metadata for later template detection
+      -- This is a closure that other functions can access
+      _G.document_meta = meta
+      return nil
+    end
+  },
+  {
+    Image = function(img)
+      return process_image(img, _G.document_meta or {})
+    end
+  }
+}
+
+-- Image Orientation Filter for Pandoc
+-- This filter handles image orientation (landscape/portrait) by generating direct LaTeX code
+-- instead of relying on template macros and Pandoc's temporary files.
+
+-- Debug function to print messages to stderr
+function debug(msg)
+  io.stderr:write("[DEBUG] Image-Orientation-Filter: " .. tostring(msg) .. "\n")
+end
+
+-- Extract filename from path
+function get_filename(path)
+  return path:match("([^/\\]+)$")
+end
+
+-- Extract extension from filename
+function get_extension(filename)
+  return filename:match("%.([^%.]+)$")
+end
+
+-- Generate a unique ID from the image filename if none is provided
+function generate_id(filename)
+  return filename:gsub("%.[^%.]+$", ""):gsub("[^%w]", "_")
+end
+
+-- Determine if we're using the academic or scientific template
+function detect_template(meta)
+  -- Check for template indicators in metadata
+  if meta.documentclass and meta.documentclass == "article" then
+    debug("Detected academic template")
+    return "academic"
+  else
+    debug("Defaulting to scientific template")
+    return "scientific"
+  end
+end
+
+-- The main filter function
+function Image(elem)
+  -- Skip processing if we're not in LaTeX format
+  if FORMAT ~= "latex" then
+    return elem
+  end
+
+  -- Extract image attributes
+  local src = elem.src
+  local title = elem.title
+  local caption = elem.caption
+  local attr = elem.attr or {}
+  local identifier = attr.identifier or ""
+  local orientation = attr.attributes.orientation or "portrait"
+  local width = attr.attributes.width or "0.8\\textwidth"
+  
+  -- Use the filename to generate an ID if none is provided
+  if identifier == "" then
+    identifier = generate_id(get_filename(src))
+  end
+  
+  -- Get original filename without path
+  local filename = get_filename(src)
+  local extension = get_extension(filename)
+  
+  -- Convert SVG to PDF if needed
+  if extension == "svg" then
+    filename = filename:gsub("%.svg$", ".pdf")
+  end
+  
+  debug("Processing image: " .. src)
+  debug("  - Orientation: " .. orientation)
+  debug("  - ID: " .. identifier)
+  debug("  - Width: " .. width)
+  debug("  - Original filename: " .. filename)
+  
+  -- Format caption as LaTeX
+  local formatted_caption = ""
+  if #caption > 0 then
+    -- Walk through the caption and convert to string
+    local caption_text = pandoc.utils.stringify(caption)
+    formatted_caption = caption_text
+  end
+  
+  -- Create direct LaTeX code based on orientation
+  local latex_code = ""
+  
+  if orientation == "landscape" then
+    debug("Generating landscape figure environment")
+    latex_code = [[
+\begin{figure}[htbp]
+  \centering
+  \begin{sidewaysfigure}
+    \centering
+    \includegraphics[width=]] .. width .. [[]{]] .. filename .. [[}
+    \caption{]] .. formatted_caption .. [[}
+    \label{fig:]] .. identifier .. [[}
+  \end{sidewaysfigure}
+\end{figure}
+]]
+  else -- portrait
+    debug("Generating portrait figure environment")
+    latex_code = [[
+\begin{figure}[htbp]
+  \centering
+  \includegraphics[width=]] .. width .. [[]{]] .. filename .. [[}
+  \caption{]] .. formatted_caption .. [[}
+  \label{fig:]] .. identifier .. [[}
+\end{figure}
+]]
+  end
+  
+  debug("Generated LaTeX code: \n" .. latex_code)
+  
+  -- Return a raw LaTeX block
+  return pandoc.RawBlock("latex", latex_code)
+end
+
+-- Main filter
+return {
+  {
+    Meta = function(meta)
+      -- Store template type in global variable for later use
+      TEMPLATE_TYPE = detect_template(meta)
+      return meta
+    end,
+    Image = Image
+  }
+}
+
 -- image-orientation-filter.lua
 -- A Pandoc Lua filter to handle image orientation in LaTeX output
 -- with proper handling of hash-based filenames
